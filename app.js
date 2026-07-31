8b630c23d15eed60e727287bc9f38e909ae3aae1
Cava <cava@Mac-mini.local>
2026-07-31 00:16:11 -0700

    Auto-publish 2026-07-31 00:16

--- app.js
+++ app.js
@@ -4203,7 +4203,7 @@ function PracticeIncomePlanner() {
     rel: /^https?:/.test(href) ? "noopener noreferrer" : null
   }, t))), /*#__PURE__*/React.createElement("div", {
     className: "sitefoot-meta"
-  }, "Last updated: July 30, 2026"), /*#__PURE__*/React.createElement("div", {
+  }, "Last updated: July 31, 2026"), /*#__PURE__*/React.createElement("div", {
     className: "sitefoot-by"
   }, /*#__PURE__*/React.createElement("b", null, "Built by Cavatello."),
     " This does not constitute legal or tax advice, just a fun simulator built with Claude.")));
